48b9636bfc763b70dc2839162f6cab864d3d9d34
hehe <hehe@cern.ch>
2020-07-14 09:09:25 -0400

    modification

--- makeScaleFactors2016.sh
+++ makeScaleFactors2016.sh
@@ -1,1 +1,1 @@
-python ScaleFactors/setupScaleFactors.py --year 2016 -t data/fakeRate15Sep2019-ZZ4l2016.root --output_file data/scaleFactorsZZ4l2016.root 
+python ScaleFactors/setupScaleFactors.py --year 2016 -t data/fakeRate15Sep2019-ZZ4l2016.root --output_file data/scaleFactorsZZ4l2016_test.root 
