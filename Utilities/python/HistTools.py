46471b4763d99f3454b80d515d5c3dd4697789f8
hehe <he.he@cern.ch>
2022-02-24 07:33:24 +0100

    Modify difference function to read from the list just created

--- Utilities/python/HistTools.py
+++ Utilities/python/HistTools.py
@@ -26,6 +26,29 @@ def getDifference(fOut, name, dir1, dir2, ratioFunc=None):
             differences.Add(ratio) 
     return differences
 
+def getDifferenceDirect(fOut, name, comp1, comp2, ratioFunc=None):
+    differences = ROOT.TList()
+    differences.SetName(name)
+    for histname in [i.GetName() for i in comp1.GetListOfKeys()]:
+        if histname == "sumweights": continue
+        hist1 = comp1.FindObject(histname)
+        hist2 = comp2.FindObject(histname)
+        if hist1 and hist2:
+            diff = hist1.Clone()
+            diff.Add(hist2, -1)
+        elif not hist1:
+            logging.warning("Hist %s was not produced for " \
+                "dataset(s) %s" % (histname, dir1))
+        elif not hist2:
+            logging.warning("Hist %s was not produced for " \
+                "dataset(s) %s" % (histname, dir2))
+        differences.Add(diff)
+    if ratioFunc:
+        ratios = ratioFunc(differences)
+        for ratio in ratios:
+            differences.Add(ratio) 
+    return differences
+
 def makeUnrolledHist(init_2D_hist, xbins, ybins, name=""):
     nbins = (len(xbins)-1)*(len(ybins)-1)
     hists_half_rolled = []
